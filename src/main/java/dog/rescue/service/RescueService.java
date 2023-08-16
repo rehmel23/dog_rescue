9a5156f95a01e35d80247c1c1644ce5198f37bba
Clay Rehmel <clay.rehmel@gmail.com>
2023-08-16 19:23:19 -0400

    Add dog-rescue code

---
+++ src/main/java/dog/rescue/service/RescueService.java
@@ -0,0 +1,38 @@
+package dog.rescue.service;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import dog.rescue.controller.model.LocationData;
+import dog.rescue.dao.LocationDao;
+import dog.rescue.entity.Location;
+
+@Service
+public class RescueService {
+
+	@Autowired
+	private LocationDao locationDao;
+
+	@Transactional(readOnly = false)
+	public LocationData saveLocation(LocationData locationData) {
+		Location location = locationData.toLocation();
+		Location dbLocation = locationDao.save(location);
+
+		return new LocationData(dbLocation);
+	}
+
+	@Transactional(readOnly = true)
+	public LocationData retrieveLocationById(Long locationId) {
+		Location location = findLocationById(locationId);
+		return new LocationData(location);
+	}
+
+	private Location findLocationById(Long locationId) {
+		return locationDao.findById(locationId)
+				.orElseThrow(() -> new NoSuchElementException("Location with ID=" + locationId + " was not found."));
+	}
+
+}
